a5683d72dc04ddc75ec2c268d5d56bba86db382a
Anserk0710 <forfun071020@gmail.com>
2025-11-07 14:45:08 +0700

    Update DB

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+
+declare global {
+  namespace globalThis {
+    var __db: PrismaClient | undefined;
+  }
+}
+
 export const db = globalThis.__db ?? new PrismaClient();
 if (process.env.NODE_ENV !== 'production') (globalThis.__db = db);
